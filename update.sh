481446175f3b2b59458faf3be7d7b0d7c28b0e54
TwIStOy <twistoy.wang@gmail.com>
2019-03-25 22:25:50 +0800

    update lmake version:

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION=$1
+
+git add --all
+git commit -m "update lmake version: ${VERSION}"
+git tag -a build-${VERSION} -m "lmake"
+git push origin master
+git push --tags
+
